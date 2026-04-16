95cca629abb7787c69b8c82242feb689c3e62f26
ThePremkumar <spremkumar2424@gmail.com>
2026-04-16 10:44:22 +0530

    create a option to choose by the user's

---
+++ main.py
@@ -0,0 +1,27 @@
+def main():
+    print("--------------------------------------")
+    print("Welcome to Prem Rentals")
+    print("--------------------------------------")
+
+    while True:
+        print("Select an Option: ")
+        print("1. Admin Panel")
+        print("2. Customer Panel")
+        print("3. Exit")
+
+        user_input = input("Enter your choice: ")
+
+        if user_input == "3":
+            print("Exiting the Main Panel")
+            break
+        elif user_input == "1":
+            print("Entering Admin Panle")
+        elif user_input == "2":
+            print("Entering Customer Panle")
+        else:
+            print("Invalid Input, Use only 1,2,3")
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/admin.py
@@ -0,0 +1,12 @@
+#Check all vehicles
+#add new vehicles
+
+
+class Admin:
+    def add_new_vehicles():
+        pass
+
+
+    
+    def view_inventory():
+        pass
