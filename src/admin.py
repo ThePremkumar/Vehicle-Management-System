94b391b9b6d94cc842221173938940fcc98b74b1
ThePremkumar <spremkumar2424@gmail.com>
2026-04-16 11:01:18 +0530

    admin can add and view the vehicles

--- src/admin.py
+++ src/admin.py
@@ -3,10 +3,30 @@
 
 
 class Admin:
-    def add_new_vehicles():
-        pass
+
+    def __init__(self, vehicles):
+        self.vehicles = vehicles
 
 
-    
-    def view_inventory():
-        pass
+    def add_new_vehicles(self, vehicle_id, model, type, rent):
+        self.vehicles[vehicle_id] = {
+            "id": vehicle_id,
+            "model": model,
+            "type": type,
+            "rent": rent,
+            "status": "available"
+        }
+        print(f"Added a Vehicle Successfully {vehicle_id}")
+
+
+
+    def view_inventory(self):
+        for value in self.vehicles.values():
+            message = f"Id: {value["id"]} | rent: {value["rent"]} | model: {value["model"]}"
+            print(value)
+
+
+        #return self.vehicles
+
+
+vehicles = {}
